ee36bb26728cb61633a865a6132190a9585b549b
Sumit-slay <143193732+Sumit-slay@users.noreply.github.com>
2024-07-21 11:42:45 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -4,12 +4,10 @@ const app = express();
 
 app.use(express.json());
 
-// Define a root route (optional)
 app.get('/', (req, res) => {
   res.send('Welcome to the Top Products Microservice!');
 });
 
-// Use /api prefix for routes
 app.use('/api', productsRoutes);
 
 const PORT = process.env.PORT || 3000;
